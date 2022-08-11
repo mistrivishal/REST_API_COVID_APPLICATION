ef3d360da3451bfa3c74a4808c82900c6041c3b0
mistrivishal <mistri.vishal95@gmail.com>
2022-08-11 18:50:28 +0530

    server port change 8080 application.properties

--- Covid-19-Vaccination/src/main/java/com/covidapp/module/Address.java
+++ Covid-19-Vaccination/src/main/java/com/covidapp/module/Address.java
@@ -1,5 +1,7 @@
 package com.covidapp.module;
 
+import javax.validation.constraints.Pattern;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +10,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Address {
-
+	
+	@Pattern(regexp="^[A-Z][a-z]*", message = "Iavalid city name - should not contain special characters.")
 	private String city;
+	
+	@Pattern(regexp="^[A-Z][a-z]*", message = "Iavalid city name - should not contain special characters.")
 	private String state;
+	
+	@Pattern(regexp="^[1-9][0-9]{5}$", message = "Iavalid pincode..")
 	private String pincde;
 }
