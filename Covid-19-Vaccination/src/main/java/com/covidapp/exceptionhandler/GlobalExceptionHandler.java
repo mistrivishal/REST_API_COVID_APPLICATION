b82c15a4faadf762225d31f5f9f52d65192a6d11
CodingShivam27 <codingshivam27@gmail.com>
2022-08-12 11:49:11 +0530

    added ExceptionHandler class and Exceptions

--- Covid-19-Vaccination/src/main/java/com/covidapp/exceptionhandler/GlobalExceptionHandler.java
+++ Covid-19-Vaccination/src/main/java/com/covidapp/exceptionhandler/GlobalExceptionHandler.java
@@ -1,5 +1,84 @@
 package com.covidapp.exceptionhandler;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.RollbackException;
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<MyErrorDetails> myExpHandlerMain(Exception ex, WebRequest wr) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(),
+									ex.getMessage(),
+									wr.getDescription(false));
+		return new ResponseEntity<MyErrorDetails>(err, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(RollbackException.class)
+	public ResponseEntity<MyErrorDetails> handleRollbackException(Exception exp, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(),
+									"Improper arguments passed in jason. Validation failed!",
+									req.getDescription(false));
+
+		return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ResponseEntity<MyErrorDetails> mynotFoundHandler(NoHandlerFoundException nfe, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(),
+									nfe.getMessage(),
+									req.getDescription(false));
+		return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<MyErrorDetails> methodArgumentNotValidException(MethodArgumentNotValidException manv,
+			WebRequest wr) {
+		String message = manv.getBindingResult().getFieldError().getDefaultMessage();
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), 
+									message,
+									wr.getDescription(false));
+		return new ResponseEntity<>(err, HttpStatus.FORBIDDEN);
+	}
+	
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<MyErrorDetails> handleValidationException(Exception exp, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(),
+									"Improper arguments passed in json. Validation failed",
+									req.getDescription(false));
+
+		return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(CenterException.class)
+	public ResponseEntity<MyErrorDetails> centerException(CenterException cexp, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(),
+									cexp.getMessage(),
+									req.getDescription(false));
+
+		return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(MemberNotFoundException.class)
+	public ResponseEntity<MyErrorDetails> memberNotFoundException(MemberNotFoundException mexp, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(),
+								mexp.getMessage(),
+								req.getDescription(false));
+
+		return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+	}
+	
+	
+	
 }
---
+++ Covid-19-Vaccination/src/main/java/com/covidapp/exceptionhandler/MemberNotFoundException.java
@@ -0,0 +1,15 @@
+package com.covidapp.exceptionhandler;
+
+
+public class MemberNotFoundException extends RuntimeException{
+
+	public MemberNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public MemberNotFoundException(String msg) {
+		// TODO Auto-generated constructor stub
+		super(msg);
+	}
+	
+}
