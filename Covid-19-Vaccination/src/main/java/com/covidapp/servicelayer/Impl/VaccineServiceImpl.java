281c968a04da54e68c5ce13ee87d535f61299fdf
Shubhambhadouria <shubhambhadouria07@gmail.com>
2022-08-12 14:38:12 +0530

    Updated VaccineImpl

--- Covid-19-Vaccination/src/main/java/com/covidapp/servicelayer/Impl/VaccineServiceImpl.java
+++ Covid-19-Vaccination/src/main/java/com/covidapp/servicelayer/Impl/VaccineServiceImpl.java
@@ -1,45 +1,68 @@
 package com.covidapp.servicelayer.Impl;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.covidapp.exceptionhandler.CenterException;
 import com.covidapp.module.Vaccine;
+import com.covidapp.repository.VaccineDao;
 import com.covidapp.servicelayer.VaccineService;
 
 public class VaccineServiceImpl implements VaccineService {
 
+	@Autowired
+	VaccineDao VacDao;
+
 	@Override
 	public List<Vaccine> allVaccine() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Vaccine> list = VacDao.findAll();
+
+		return list;
 	}
 
 	@Override
 	public Vaccine getVaccinebyName(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		Vaccine vac = VacDao.findByVaccineName(name);
+		if (vac == null) {
+			throw new CenterException("Vaccine not found");
+		}
+
+		return vac;
 	}
 
 	@Override
 	public Vaccine getVaccineId(Integer vaccineid) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return VacDao.findById(vaccineid).orElseThrow(new VaccineNotFoundException("Vaccine not Found"));
+		
 	}
 
 	@Override
 	public Vaccine addVaccine(Vaccine vaccine) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return 	VacDao.save(vaccine);
 	}
 
 	@Override
 	public Vaccine updateVaccine(Vaccine vaccine) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Vaccine> opt = VacDao.findById(vaccine.getVaccineId());
+		if(opt.isPresent()) {
+			VacDao.save(vaccine);
+		}
+		else {
+			throw new VaccineNotFoundException("Vaccine not found");
+		}
 	}
 
 	@Override
 	public Boolean deleteVaccine(Vaccine vaccine) {
-		// TODO Auto-generated method stub
+		Optional<Vaccine> opt = VacDao.findById(vaccine.getVaccineId());
+		if(opt.isPresent()) {
+			VacDao.delete(vaccine);
+			return true;
+		}
 		return false;
 	}
 
