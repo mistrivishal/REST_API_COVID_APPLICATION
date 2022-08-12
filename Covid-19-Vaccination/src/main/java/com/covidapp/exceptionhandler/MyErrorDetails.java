6addbf0f688f55ee35433b73c86dcfdf29e731b9
CodingShivam27 <codingshivam27@gmail.com>
2022-08-12 10:49:33 +0530

    added ErrorDetails class

---
+++ Covid-19-Vaccination/src/main/java/com/covidapp/exceptionhandler/MyErrorDetails.java
@@ -0,0 +1,18 @@
+package com.covidapp.exceptionhandler;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MyErrorDetails {
+
+	private LocalDateTime timestamp;
+	private String message;
+	private String details;
+	
+}
