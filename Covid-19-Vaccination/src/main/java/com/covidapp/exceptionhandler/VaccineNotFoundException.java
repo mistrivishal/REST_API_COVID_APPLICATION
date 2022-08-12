da75c64ba2ca32dde1e1bf2638b8039dca1b28cd
CodingShivam27 <codingshivam27@gmail.com>
2022-08-12 19:20:00 +0530

    added vaccineNotFoundException in exceptionHandler

---
+++ Covid-19-Vaccination/src/main/java/com/covidapp/exceptionhandler/VaccineNotFoundException.java
@@ -0,0 +1,15 @@
+package com.covidapp.exceptionhandler;
+
+public class VaccineNotFoundException extends RuntimeException{
+
+	public VaccineNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public VaccineNotFoundException(String msg) {
+		// TODO Auto-generated constructor stub
+		super(msg);
+		
+	}
+	
+}
--- Covid-19-Vaccination/src/main/java/com/covidapp/servicelayer/Impl/VaccineServiceImpl.java
+++ Covid-19-Vaccination/src/main/java/com/covidapp/servicelayer/Impl/VaccineServiceImpl.java
@@ -1,11 +1,13 @@
 package com.covidapp.servicelayer.Impl;
 
 import java.util.List;
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.covidapp.exceptionhandler.CenterException;
+import com.covidapp.exceptionhandler.VaccineNotFoundException;
 import com.covidapp.module.Vaccine;
 import com.covidapp.repository.VaccineDao;
 import com.covidapp.servicelayer.VaccineService;
@@ -13,18 +15,18 @@ import com.covidapp.servicelayer.VaccineService;
 public class VaccineServiceImpl implements VaccineService {
 
 	@Autowired
-	VaccineDao VacDao;
+	private VaccineDao vacDao;
 
 	@Override
 	public List<Vaccine> allVaccine() {
-		List<Vaccine> list = VacDao.findAll();
+		List<Vaccine> list = vacDao.findAll();
 
 		return list;
 	}
 
 	@Override
 	public Vaccine getVaccinebyName(String name) {
-		Vaccine vac = VacDao.findByVaccineName(name);
+		Vaccine vac = vacDao.findByVaccineName(name);
 		if (vac == null) {
 			throw new CenterException("Vaccine not found");
 		}
@@ -35,21 +37,28 @@ public class VaccineServiceImpl implements VaccineService {
 	@Override
 	public Vaccine getVaccineId(Integer vaccineid) {
 		
-		return VacDao.findById(vaccineid).orElseThrow(new VaccineNotFoundException("Vaccine not Found"));
+		Optional<Vaccine> vac=vacDao.findById(vaccineid);
 		
+		if(vac.isPresent()) {
+			return vac.get();
+		}
+		else {
+			throw new VaccineNotFoundException("Vaccine not found");
+		}
 	}
 
 	@Override
 	public Vaccine addVaccine(Vaccine vaccine) {
 		
-		return 	VacDao.save(vaccine);
+		return 	vacDao.save(vaccine);
 	}
 
 	@Override
 	public Vaccine updateVaccine(Vaccine vaccine) {
-		Optional<Vaccine> opt = VacDao.findById(vaccine.getVaccineId());
+		Optional<Vaccine> opt = vacDao.findById(vaccine.getVaccineId());
 		if(opt.isPresent()) {
-			VacDao.save(vaccine);
+			return vacDao.save(vaccine);
+			
 		}
 		else {
 			throw new VaccineNotFoundException("Vaccine not found");
@@ -58,9 +67,9 @@ public class VaccineServiceImpl implements VaccineService {
 
 	@Override
 	public Boolean deleteVaccine(Vaccine vaccine) {
-		Optional<Vaccine> opt = VacDao.findById(vaccine.getVaccineId());
+		Optional<Vaccine> opt = vacDao.findById(vaccine.getVaccineId());
 		if(opt.isPresent()) {
-			VacDao.delete(vaccine);
+			vacDao.delete(vaccine);
 			return true;
 		}
 		return false;
